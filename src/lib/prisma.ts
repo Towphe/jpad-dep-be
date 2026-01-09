e9bc4a5619c5e64962a7ddcdea284518785dfc71
Towphe <castillochristopherjames@gmail.com>
2026-01-09 09:05:19 +0800

    chore: created global prisma instance

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,19 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../prisma/prisma/client.js";
 import config from "../config/config.js";
+import { PrismaClient } from "../../prisma/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+declare global {
+  // allow attaching prisma to the global object during development
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
 
 const connectionString = `${config.dbUrl}`;
 
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
 
-export { prisma };
+const prisma = global.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+export default prisma;
